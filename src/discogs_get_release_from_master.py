3ca4444936e5c2694ac72f4691df004d704f1b36
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-04 09:05:18 +0200

    fix: add traceback on error discogs_get_release_from_master.py

--- src/discogs_get_release_from_master.py
+++ src/discogs_get_release_from_master.py
@@ -1,10 +1,11 @@
 # components
-from env import *
 from discogs_sleep import DiscogsSleep
 
 # packages
 import requests
 import json
+import logging
+import traceback
 
 def DiscogsGetReleaseFromMaster(master_id):
     DiscogsSleep()
@@ -12,8 +13,11 @@ def DiscogsGetReleaseFromMaster(master_id):
     base_url = 'https://api.discogs.com/masters/'
     response = requests.get(base_url + master_id)
     response_json = json.loads(response.text)
-    
-    release_id = response_json['main_release_url'].rsplit('/', 1)[1]
+    release_id = None
+
+    try:
+        release_id = response_json['main_release_url'].rsplit('/', 1)[1]
+    except Exception as e:
+        logging.error(traceback.format_exc())
 
     return release_id
-
