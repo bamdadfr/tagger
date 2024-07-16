a2cfa94fc5d656e184bbd404c4af0b2cdb5462f1
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-16 23:09:10 +0200

    fix: add privileges to scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t tagger .
+sudo docker build -t tagger .
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --mount type=bind,source=/home/bamdad/git/tagger/config.yaml,target=/app/config.yaml --mount type=bind,source=/,target=/mount tagger
+sudo docker run -it --rm --mount type=bind,source=/home/bamdad/git/tagger/config.yaml,target=/app/config.yaml --mount type=bind,source=/,target=/mount tagger
